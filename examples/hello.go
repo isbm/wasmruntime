8dec158c849d66c9f897d4a3a15b35252ce670f5
Bo Maryniuk <bogdan.maryniuk@esolutions.de>
2025-09-23 14:35:13 +0200

    Add file writing from the sandbox

--- examples/hello.go
+++ examples/hello.go
@@ -9,8 +9,16 @@ import (
 
 func main() {
 	msg := map[string]any{
-		"ok":  true,
-		"msg": "hello world from Go (Normal and Tiny)",
+		"ok":     true,
+		"msg":    "hello world from Go (Normal and Tiny)",
+		"result": "Looks OK",
+	}
+
+	s := "Some text here.\n"
+	err := os.WriteFile("golang-output.txt", []byte(s), 0644)
+	if err != nil {
+		msg["ok"] = false
+		msg["result"] = err.Error()
 	}
 
 	data, err := json.Marshal(msg)
